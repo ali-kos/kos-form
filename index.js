723f0c018fbfa84a634074de19a671e5cddbb57f
吉星 <shihong.xsh@alibaba-inc.com>
2019-04-03 15:08:09 +0800

    feat: 支持FormHOC

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import xForm from './src/xform/form';
-import xField from './src/xform/field';
-import FormMiddleware from './src/form-middleware';
-import * as ValidatorRule from './src/form-validator/validator-rule';
+import React from "react";
+import xForm from "./src/xform/form";
+import xField from "./src/xform/field";
+import FormMiddleware from "./src/form-middleware";
+import * as ValidatorRule from "./src/form-validator/validator-rule";
 
-export const Form = xForm;
+export const Form = xForm();
 export const formMiddleware = FormMiddleware;
 export const FieldHOC = xField;
+export const FormHOC = xForm;
 export const addRule = ValidatorRule.addRule;
 export const removeRule = ValidatorRule.removeRule;
-
--- src/xform/form.js
+++ src/xform/form.js
@@ -58,310 +58,313 @@ const getFormData = (namespace, formName) =>
 const resetForm = (namespace, formName) =>
   runFormMethod(namespace, formName, "reset");
 
-class Form extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    onSubmit: PropTypes.func,
-    dispatch: PropTypes.func
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.fieldList = [];
-  }
-
-  dispatch(action) {
-    const dispatch = this.props.dispatch || this.context.dispatch;
-
-    dispatch && dispatch(action);
-  }
-  getState() {
-    const { getState } = this.context;
-    if (getState) {
-      return getState();
-    }
-
-    return this.props;
-  }
-  /**
-   * 根据字段名称，获取字段的展示数据
-   * @param {String} field 字段名称
-   */
-  getFieldDisplayData(field) {
-    const { name: formName } = this.props;
-    const state = this.getState();
-    return getFieldDisplayData(state, formName, field);
-  }
-  /**
-   * 根据字段名称，获取字段的校验数据
-   * @param {String} field 字段名称
-   */
-  getFieldVaidateData(field) {
-    const { name: formName } = this.props;
-    const state = this.getState();
-    return getFieldValidateData(state, formName, field);
-  }
-  /**
-   * 字段值发生变化时触发
-   * @param {String} field 字段名称，表单内不可重复
-   * @param {String} value 值
-   * @param {String} vField 字段类型，表单内可重复
-   */
-  onFieldChange({ field, value, vField, required, validator }) {
-    const { name: formName } = this.props;
-
-    const payload = {
-      field,
-      vField,
-      value,
-      formName,
-      required,
-      validator
+export default (FormWrapper = 'form') => {
+  class Form extends React.Component {
+    static propTypes = {
+      name: PropTypes.string.isRequired,
+      onSubmit: PropTypes.func,
+      dispatch: PropTypes.func
     };
 
-    // 受控组件：传递值
-    this.dispatch({
-      type: XFORM_FIELD_CHANGE,
-      payload
-    });
+    constructor(props) {
+      super(props);
 
-    // 执行表单校验，连续操作只触发一次校验
-    if (this.fieldChangeValidateSeed) {
-      clearTimeout(this.fieldChangeValidateSeed);
+      this.fieldList = [];
     }
-    this.fieldChangeValidateSeed = setTimeout(() => {
-      this.validateField(payload);
-    }, 400);
-  }
 
-  onFieldFocus({ field, vField }) {}
-  onFieldBlur() {}
-  onFieldKeyUp() {}
-  onFieldDown() {}
-  /**
-   * 获取表单值
-   */
-  getData() {
-    const { name: formName } = this.props;
-    const state = this.getState();
+    dispatch(action) {
+      const dispatch = this.props.dispatch || this.context.dispatch;
 
-    return state[formName] || {};
-  }
-  reset() {
-    const namespace = this.context.getNamespace();
-    const { name: formName } = this.props;
-    const Model = KOS.getModel(namespace);
-
-    const initialModel = Model.getInitial();
-    this.setData(initialModel[formName]);
-  }
-  setData(formData) {
-    const { name: formName } = this.props;
-
-    this.dispatch({
-      type: "setState",
-      payload: {
-        [formName]: formData
+      dispatch && dispatch(action);
+    }
+    getState() {
+      const { getState } = this.context;
+      if (getState) {
+        return getState();
       }
-    });
-  }
-  /**
-   * 根据字段名称，获取字段的值
-   * @param {String} field 字段名称
-   */
-  getFieldValue(field) {
-    const formData = this.getData();
-    return formData[field];
-  }
-  getFieldPropsList() {
-    return this.fieldList.map(fieldComponent => fieldComponent.props);
-  }
-  /**
-   * 执行全表单校验
-   */
-  validate(callback) {
-    const { name: formName } = this.props;
-    const { fieldList } = this;
 
-    const fieldPropsList = this.getFieldPropsList();
-    this.dispatch({
-      type: XFORM_VALIDATE,
-      payload: {
-        formName,
-        fieldPropsList,
-        callback
-      }
-    });
-  }
-  /**
-   * 执行表单字段校验
-   * @param {String|Object} field 校验的参数
-   * @param {Function} callback 回调方法
-   */
-  validateField(field, callback) {
-    const payload = {
-      value: this.getFieldValue(field),
-      formName: this.props.name,
-      callback
-    };
+      return this.props;
+    }
+    /**
+     * 根据字段名称，获取字段的展示数据
+     * @param {String} field 字段名称
+     */
+    getFieldDisplayData(field) {
+      const { name: formName } = this.props;
+      const state = this.getState();
+      return getFieldDisplayData(state, formName, field);
+    }
+    /**
+     * 根据字段名称，获取字段的校验数据
+     * @param {String} field 字段名称
+     */
+    getFieldVaidateData(field) {
+      const { name: formName } = this.props;
+      const state = this.getState();
+      return getFieldValidateData(state, formName, field);
+    }
+    /**
+     * 字段值发生变化时触发
+     * @param {String} field 字段名称，表单内不可重复
+     * @param {String} value 值
+     * @param {String} vField 字段类型，表单内可重复
+     */
+    onFieldChange({ field, value, vField, required, validator }) {
+      const { name: formName } = this.props;
 
-    if (typeof field === "object") {
-      Object.assign(payload, field);
-    } else {
-      Object.assign(payload, {
+      const payload = {
         field,
-        vField: field
+        vField,
+        value,
+        formName,
+        required,
+        validator
+      };
+
+      // 受控组件：传递值
+      this.dispatch({
+        type: XFORM_FIELD_CHANGE,
+        payload
+      });
+
+      // 执行表单校验，连续操作只触发一次校验
+      if (this.fieldChangeValidateSeed) {
+        clearTimeout(this.fieldChangeValidateSeed);
+      }
+      this.fieldChangeValidateSeed = setTimeout(() => {
+        this.validateField(payload);
+      }, 400);
+    }
+
+    onFieldFocus({ field, vField }) {}
+    onFieldBlur() {}
+    onFieldKeyUp() {}
+    onFieldDown() {}
+    /**
+     * 获取表单值
+     */
+    getData() {
+      const { name: formName } = this.props;
+      const state = this.getState();
+
+      return state[formName] || {};
+    }
+    reset() {
+      const namespace = this.context.getNamespace();
+      const { name: formName } = this.props;
+      const Model = KOS.getModel(namespace);
+
+      const initialModel = Model.getInitial();
+      this.setData(initialModel[formName]);
+    }
+    setData(formData) {
+      const { name: formName } = this.props;
+
+      this.dispatch({
+        type: "setState",
+        payload: {
+          [formName]: formData
+        }
       });
     }
-
-    this.dispatch({
-      type: XFORM_FIELD_VALIDATE,
-      payload
-    });
-  }
-  /**
-   * 禁用字段校验
-   * @param {String} field 字段名
-   * @param {Boolean} disabled 是否禁用
-   */
-  disableFieldValidator(field, disabled) {
-    const { name: formName } = this.props;
-    this.dispatch({
-      type: XFORM_FIELD_VALIDATOR_DISABLE,
-      payload: {
-        field,
-        formName,
-        disabled
-      }
-    });
-  }
-  /**
-   * 禁用字段校验的指定规则
-   * @param {String} field 字段名
-   * @param {String} rule 校验规则，可以是校验规则名，也可以是校验规则的index，从0开始
-   * @param {Boolean} disabled 是否禁用
-   */
-  disableFieldValidatorRule(field, rule, disabled) {
-    const { name: formName } = this.props;
-    this.dispatch({
-      type: XFORM_FIELD_VALIDATOR_RULE_DISABLE,
-      payload: {
-        field,
-        formName,
-        rule,
-        disabled
-      }
-    });
-  }
-  clearValidate() {
-    const { name: formName } = this.props;
-    const fieldPropsList = this.getFieldPropsList();
-
-    this.dispatch({
-      type: XFORM_CLEAR_VALIDATE,
-      payload: { formName, fieldPropsList }
-    });
-  }
-  clearFieldValidate(field) {
-    const { name: formName } = this.props;
-    this.dispatch({
-      type: XFORM_CLEAR_FIELD_VALIDATE,
-      payload: { formName, field }
-    });
-  }
-  onSubmit() {
-    const { onSubmit } = this.props;
-
-    this.validate(result => {
+    /**
+     * 根据字段名称，获取字段的值
+     * @param {String} field 字段名称
+     */
+    getFieldValue(field) {
       const formData = this.getData();
-      result && onSubmit && onSubmit(formData);
-    });
+      return formData[field];
+    }
+    getFieldPropsList() {
+      return this.fieldList.map(fieldComponent => fieldComponent.props);
+    }
+    /**
+     * 执行全表单校验
+     */
+    validate(callback) {
+      const { name: formName } = this.props;
+      const { fieldList } = this;
 
-    return false;
-  }
-  registerField(fieldComponent) {
-    // 此处保存所有的Props
-    this.fieldList.push(fieldComponent);
-  }
-  revokeField(key) {
-    const { fieldList } = this;
-    let index = 0;
-    const len = fieldList.length;
+      const fieldPropsList = this.getFieldPropsList();
+      this.dispatch({
+        type: XFORM_VALIDATE,
+        payload: {
+          formName,
+          fieldPropsList,
+          callback
+        }
+      });
+    }
+    /**
+     * 执行表单字段校验
+     * @param {String|Object} field 校验的参数
+     * @param {Function} callback 回调方法
+     */
+    validateField(field, callback) {
+      const payload = {
+        value: this.getFieldValue(field),
+        formName: this.props.name,
+        callback
+      };
 
-    while (index < len) {
-      const fieldProps = fieldList[index];
-      if (key === fieldProps.key) {
-        break;
+      if (typeof field === "object") {
+        Object.assign(payload, field);
+      } else {
+        Object.assign(payload, {
+          field,
+          vField: field
+        });
       }
-      index++;
+
+      this.dispatch({
+        type: XFORM_FIELD_VALIDATE,
+        payload
+      });
     }
-    if (index < len) {
-      this.fieldList.splice(index, 1);
+    /**
+     * 禁用字段校验
+     * @param {String} field 字段名
+     * @param {Boolean} disabled 是否禁用
+     */
+    disableFieldValidator(field, disabled) {
+      const { name: formName } = this.props;
+      this.dispatch({
+        type: XFORM_FIELD_VALIDATOR_DISABLE,
+        payload: {
+          field,
+          formName,
+          disabled
+        }
+      });
+    }
+    /**
+     * 禁用字段校验的指定规则
+     * @param {String} field 字段名
+     * @param {String} rule 校验规则，可以是校验规则名，也可以是校验规则的index，从0开始
+     * @param {Boolean} disabled 是否禁用
+     */
+    disableFieldValidatorRule(field, rule, disabled) {
+      const { name: formName } = this.props;
+      this.dispatch({
+        type: XFORM_FIELD_VALIDATOR_RULE_DISABLE,
+        payload: {
+          field,
+          formName,
+          rule,
+          disabled
+        }
+      });
+    }
+    clearValidate() {
+      const { name: formName } = this.props;
+      const fieldPropsList = this.getFieldPropsList();
+
+      this.dispatch({
+        type: XFORM_CLEAR_VALIDATE,
+        payload: { formName, fieldPropsList }
+      });
+    }
+    clearFieldValidate(field) {
+      const { name: formName } = this.props;
+      this.dispatch({
+        type: XFORM_CLEAR_FIELD_VALIDATE,
+        payload: { formName, field }
+      });
+    }
+    onSubmit() {
+      const { onSubmit } = this.props;
+
+      this.validate(result => {
+        const formData = this.getData();
+        result && onSubmit && onSubmit(formData);
+      });
+
+      return false;
+    }
+    registerField(fieldComponent) {
+      // 此处保存所有的Props
+      this.fieldList.push(fieldComponent);
+    }
+    revokeField(key) {
+      const { fieldList } = this;
+      let index = 0;
+      const len = fieldList.length;
+
+      while (index < len) {
+        const fieldProps = fieldList[index];
+        if (key === fieldProps.key) {
+          break;
+        }
+        index++;
+      }
+      if (index < len) {
+        this.fieldList.splice(index, 1);
+      }
+    }
+    getChildContext() {
+      return {
+        registerField: this.registerField.bind(this),
+        revokeField: this.revokeField.bind(this),
+        onFieldChange: this.onFieldChange.bind(this),
+        getFieldValue: this.getFieldValue.bind(this),
+        getFieldVaidateData: this.getFieldVaidateData.bind(this),
+        getFieldDisplayData: this.getFieldDisplayData.bind(this)
+      };
+    }
+    componentDidMount() {
+      addForm(this);
+    }
+    componentWillUnmount() {
+      const namespace = this.context.getNamespace();
+      const { name: formName } = this.props;
+
+      removeForm(namespace, formName);
+    }
+    render() {
+      const { children } = this.props;
+
+      return (
+        <FormWrapper
+          {...this.props}
+          onSubmit={() => {
+            return this.onSubmit();
+          }}
+        >
+          {children}
+        </FormWrapper>
+      );
     }
   }
-  getChildContext() {
-    return {
-      registerField: this.registerField.bind(this),
-      revokeField: this.revokeField.bind(this),
-      onFieldChange: this.onFieldChange.bind(this),
-      getFieldValue: this.getFieldValue.bind(this),
-      getFieldVaidateData: this.getFieldVaidateData.bind(this),
-      getFieldDisplayData: this.getFieldDisplayData.bind(this)
-    };
-  }
-  componentDidMount() {
-    addForm(this);
-  }
-  componentWillUnmount() {
-    const namespace = this.context.getNamespace();
-    const { name: formName } = this.props;
 
-    removeForm(namespace, formName);
-  }
-  render() {
-    const { children } = this.props;
-    return (
-      <form
-        {...this.props}
-        onSubmit={() => {
-          return this.onSubmit();
-        }}
-      >
-        {children}
-      </form>
-    );
-  }
-}
+  // 传递给field用的
+  Form.childContextTypes = {
+    registerField: PropTypes.func,
+    revokeField: PropTypes.func,
+    onFieldChange: PropTypes.func,
+    getFieldValue: PropTypes.func,
+    getFieldVaidateData: PropTypes.func,
+    getFieldDisplayData: PropTypes.func
+  };
 
-// 传递给field用的
-Form.childContextTypes = {
-  registerField: PropTypes.func,
-  revokeField: PropTypes.func,
-  onFieldChange: PropTypes.func,
-  getFieldValue: PropTypes.func,
-  getFieldVaidateData: PropTypes.func,
-  getFieldDisplayData: PropTypes.func
+  // 来自wrapper
+  Form.contextTypes = {
+    dispatch: PropTypes.func,
+    getState: PropTypes.func,
+    getNamespace: PropTypes.func
+  };
+
+  Form.getForm = getForm;
+  Form.getFormData = getFormData;
+  Form.resetForm = resetForm;
+  Form.validate = validate;
+
+  KOS.wrapperProps({
+    getForm(formName) {
+      const namespace = this.getNamespace();
+
+      return Form.getForm(namespace, formName);
+    }
+  });
+
+  return Form;
 };
-
-// 来自wrapper
-Form.contextTypes = {
-  dispatch: PropTypes.func,
-  getState: PropTypes.func,
-  getNamespace: PropTypes.func
-};
-
-Form.getForm = getForm;
-Form.getFormData = getFormData;
-Form.resetForm = resetForm;
-Form.validate = validate;
-
-KOS.wrapperProps({
-  getForm(formName) {
-    const namespace = this.getNamespace();
-
-    return Form.getForm(namespace, formName);
-  }
-});
-
-export default Form;
